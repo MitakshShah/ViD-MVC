7a73084955114078f1a3aa5c6d9626e10062ae73
Mitax <mitaxshah@gmail.com>
2018-11-22 16:49:37 -0500

    set up models and views

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,18 +1,22 @@
 <?php 
 
-class Pages {
+class Pages extends Controller {
     
     public function __construct() {
         
     }
 
     public function index() {
-        
+        $data = [
+            'title' => 'welcome'
+        ];
+        $this->view('pages/index', $data);
     }
 
-    public function about($id) {
-        // echo 'This is about';
-        echo $id;
-
+    public function test() {
+        $data = [
+            'title' => 'Test'
+        ];
+        $this->view('pages/test', $data);
     }
 }
