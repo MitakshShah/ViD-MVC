c075b6347cf9a01b3e00a246772ef1aa51dc20b4
Mitax <mitaxshah@gmail.com>
2018-11-22 16:50:37 -0500

    autoload core libraries instead of manually

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 
-// Load libraries
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Db.php';
+// Load Config file
+require_once 'config/config.php';
+
+// Autoload Core libraries
+spl_autoload_register(function($className){
+    require_once 'libraries/' . $className . '.php';
+});
