c1f58da64a3fd0f9c2ad2b2763b3a58b43888f5e
Mitax <mitaxshah@gmail.com>
2018-11-22 16:46:02 -0500

    adding site urls in config file

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// App Root
+define('APP_ROOT',dirname(dirname(__FILE__)));
+
+// URL Root
+define('URL_ROOT', 'http://localhost/vid-mvc');
+
+// Site Name
+define('SITE_NAME', 'ViD MVC Framwork - PHP');
